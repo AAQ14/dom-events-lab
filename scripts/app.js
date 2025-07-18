3332fecb7555aa87fd1d6d90b1af3c88ab05438c
AAQ14 <80754496+AAQ14@users.noreply.github.com>
2025-07-18 13:45:22 +0300

    sumbit dom events lab

---
+++ scripts/app.js
@@ -0,0 +1,53 @@
+function init() {
+    /*------------------------------------Constants----------------------------------- */
+
+    /*-----------------------------------variables---------------------------------- */
+    let current = ''
+    let result = ''
+
+    /*----------------------------------Cached Element References--------------------------------- */
+    const buttons = document.querySelectorAll('.button');
+    const displayElm = document.querySelector('.display');
+  
+
+    /*----------------------------------Functions--------------------------------- */
+
+    function clear() {
+        displayElm.textContent = "";
+        current = ''
+    }
+
+    //Evaluate the value on current string
+    function displayResult() {
+        result = eval(current)
+        displayElm.textContent = result;
+
+        console.log(result)
+    }
+
+    /*----------------------------------Event listeners--------------------------------- */
+
+    buttons.forEach(button => {
+        button.addEventListener('click', (event) => {
+        //check the button clicked if its not = and C than add the value to current
+        if (event.target.innerText !== "=" && event.target.innerText !== "C") {
+            displayElm.textContent += event.target.innerText;
+            current += event.target.innerText
+            console.log(current)
+        }
+
+        //clear the calculator
+        if (event.target.innerText === "C") {
+            clear()
+        }
+
+        // if user click on equal than display the result
+        if (event.target.innerText === "=") {
+            displayElm.textContent += event.target.innerText;
+            displayResult()
+        }
+    });
+    })
+
+}
+document.addEventListener('DOMContentLoaded', init)
